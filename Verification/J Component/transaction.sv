14664894c61c1a93cc4ed415c58ea2490654294a
SHREYAS S BAGI <37632294+melonshreyas@users.noreply.github.com>
2024-12-18 22:05:08 +0530

    Update transaction.sv

--- Verification/J Component/transaction.sv
+++ Verification/J Component/transaction.sv
@@ -7,6 +7,13 @@ class transaction;
   bit [8:0] Bus;
   bit Done;
   bit [8:0] temp_add_res,temp_sub_res,temp_mvi,temp_mv;
+  /* 
+  The constraints are made to generate only one opcode here. Modify to generate all. 
+  Initial we want the register to be filled with some content then only we can do or carry out other operation otherwise all will be in High-impendance state.
+  Refer the Simple Processor J Component Report.pdf
+  That's the reason we are generating the mv opcode only. 
+  Whereas the Register are from R0 to R7. 
+  */
   constraint din {DIN[8:6] >= 3'd0;DIN[8:6] < 3'd1;DIN[5:3]>= 3'd0;DIN[5:3]<= 3'd7;DIN[2:0]>= 3'd0;DIN[2:0]<= 3'd7;}
   //constraint run {Run > 1'd0;Run<2'd2;}
 
